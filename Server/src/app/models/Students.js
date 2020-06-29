5677e200cbf60a5d8827cbb8b9d0045ea78aed71
Igor Tuag <igortuag@gmail.com>
2020-06-29 18:43:07 -0300

    Create models and integrate with Sequelize

---
+++ Server/src/app/models/Students.js
@@ -0,0 +1,20 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Students extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        age: Sequelize.INTEGER,
+        weight: Sequelize.FLOAT,
+        height: Sequelize.FLOAT,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default Students;
