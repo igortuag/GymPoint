49154ff2ba6f0a2193bb47903fa022e4abbd3938
Igor Tuag <igortuag@gmail.com>
2020-06-28 21:47:59 -0300

    Instal sucrase and nodemon

--- Server/src/routes.js
+++ Server/src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  return res.json({ message: "Hello GymPoint" });
+  return res.json({ message: "Hello GymPoint whith sucrase + nodemon" });
 });
 
-module.exports = routes;
+export default routes;
