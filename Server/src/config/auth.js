8678dd6e1dcbd69146cc088d61349cf97a31e00a
Igor Tuag <igortuag@gmail.com>
2020-06-29 19:42:17 -0300

    Auth using JWT token

---
+++ Server/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'bf2be738a256681bc51856a18b68992a',
+  expireIn: '7d',
+};
--- Server/src/routes.js
+++ Server/src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import StudentController from './app/controllers/StudentController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 routes.post('/students', StudentController.store);
 
