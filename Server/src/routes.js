423208a1e6da432ae9bd85ae0a03c4810fc6af38
Igor Tuag <igortuag@gmail.com>
2020-06-28 22:14:30 -0300

    Add Eslint, Prettier & EditorConfig

--- Server/src/routes.js
+++ Server/src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello GymPoint whith sucrase + nodemon" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello GymPoint whith sucrase + nodemon' });
 });
 
 export default routes;
