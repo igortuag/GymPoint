f8c72698f165c1625f835b5cf08c743d8450211b
Igor Tuag <igortuag@gmail.com>
2020-06-29 20:04:15 -0300

    Middleware auth JWT token

--- Server/src/routes.js
+++ Server/src/routes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+
 import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
+
 import StudentController from './app/controllers/StudentController';
 import SessionController from './app/controllers/SessionController';
 
@@ -7,6 +10,8 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.post('/students', StudentController.store);
 
