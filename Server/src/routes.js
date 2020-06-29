dc325cc90d1e2f3fc936fe0ac6d8d15d89c75973
Igor Tuag <igortuag@gmail.com>
2020-06-29 18:59:33 -0300

    Create connection database with routes

--- Server/src/routes.js
+++ Server/src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import User from './app/models/User';
+import Student from './app/models/Student';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello GymPoint whith sucrase + nodemon' });
+routes.get('/', async (req, res) => {
+  const student = await Student.create({
+    name: 'Igor Tuag',
+    email: 'igortuag@gmail.com',
+    age: 25,
+    weight: 70,
+    height: 1.68,
+  });
+  return res.json(student);
 });
 
 export default routes;
